926e29071db473175f17294273a873c0d1d0d738
Pauline Menage <pmenage@212.195.251.212.customer.cdi.no>
2017-07-29 13:26:37 +0200

    Added gitignore

--- set-webhook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-curl -X POST \
-  -H "Content-Type: application/json" \
-  -d '{
-     "url": "https://us-central1-uc-internal-sandbox.cloudfunctions.net/bot"
-   }' \
-   https://api.telegram.org/bot404221654:AAHh87fMJ5_Y_7Bj29anw0H2cNXSxbmp4ig/setWebhook
